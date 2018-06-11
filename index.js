6409548df13d075c07fe7b3d6fea1dceb10f97df
Melanie Downing <sayanything830@gmail.com>
2018-06-11 11:44:32 -0700

    heroku debug

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const app = express();
 require('dotenv').config();
 
 app.use(express.static('./src/public'));
+
+app.get('*',(request,response) => {
+  response.sendFile(`${__dirname}/build/index.html`);
+});
+
+app.listen(process.env.PORT,() => {
+  console.log('__SERVER_UP__',process.env.PORT);
+});
